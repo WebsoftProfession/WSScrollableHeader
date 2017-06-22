24c4b590bc1628419270785facf257cf7622f156
WebsoftProfession <dotsquares@praveens-mac-mini.local>
2017-06-22 11:17:13 +0530

    Initial Commit

---
+++ WSScrollableHeader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WSScrollableHeader
+//
+//  Created by Dotsquares on 6/22/17.
+//  Copyright © 2017 WebsoftProfession. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WSScrollableHeader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WSScrollableHeader
+//
+//  Created by Dotsquares on 6/22/17.
+//  Copyright © 2017 WebsoftProfession. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
